29242e3d87e682904e6a9e03422a7b55e66d3cec
William Dantas <dantaswilliam@gmail.com>
2024-09-03 03:01:09 -0300

    [Feat]: refatoração home

--- lib/features/presentation/screens/home_screen.dart
+++ lib/features/presentation/screens/home_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
-import 'package:pokedex/core/controllers/home_controller.dart';
-import 'package:pokedex/features/presentation/stores/pokemon_store.dart';
 import 'package:lottie/lottie.dart';
-import 'package:pokedex/features/presentation/widgets/header_widget.dart';
+import 'package:pokedex/core/presenters/theme/size_outlet.dart';
 
-import '../../../core/presenters/theme/size_outlet.dart';
+import '../../../core/controllers/home_controller.dart';
 import '../widgets/card_pokemon_widget.dart';
+import '../widgets/header_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,57 +16,71 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final pokemonStore = GetIt.instance<PokemonStore>();
   final homeController = GetIt.instance<HomeController>();
 
-  @override
-  void initState() {
-    super.initState();
-    pokemonStore.fetchPokemons();
+  // Método para construir o indicador de carregamento
+  Widget _buildLoadingIndicator() {
+    return Center(
+      child: Lottie.asset(
+        'assets/lottie/pokeball_loading.json',
+        width: SizeOutlet.imageSizeLarge,
+        height: SizeOutlet.imageSizeLarge,
+      ),
+    );
+  }
+
+  // Método para construir o estado vazio
+  Widget _buildEmptyState() {
+    return const Center(
+      child: Text(
+        'No Pokemons Found',
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  // Método para construir a lista de pokémons
+  Widget _buildPokemonList() {
+    return ListView.builder(
+      itemCount: homeController.listaPokemon.length,
+      itemBuilder: (context, index) {
+        final pokemon = homeController.listaPokemon[index];
+        return CardPokemon(pokemon: pokemon);
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Observer(
-          builder: (_) {
-            return Column(
-              children: [
-                Header(
-                  controllerSearch: homeController.controllerSearch,
-                  title: 'Pokedex',
-                  onChanged: (_) => homeController.searchPokemon(),
-                ),
-                Expanded(
-                  child: Observer(builder: (context) {
-                    if (homeController.isLoadList == false) {
-                      return Center(
-                        child: Lottie.asset(
-                          'assets/lottie/pokeball_loading.json',
-                          width: SizeOutlet.imageSizeLarge,
-                          height: SizeOutlet.imageSizeLarge,
-                        ),
-                      );
-                    }
-                    if (homeController.listaPokemon.isEmpty) {
-                      return Center(child: Text('Error: ${pokemonStore.error}'));
-                    }
-                    if (homeController.listaPokemon.isEmpty) {
-                      return const Center(child: Text('No Pokemons Found'));
-                    }
-                    return ListView.builder(
-                      itemCount: homeController.listaPokemon.length,
-                      itemBuilder: (context, index) {
-                        final pokemon = homeController.listaPokemon[index];
-                        return CardPokemon(pokemon: pokemon);
-                      },
-                    );
-                  }),
-                ),
-              ],
-            );
-          }
+        child: Column(
+          children: [
+            // Header com barra de pesquisa
+            Header(
+              controllerSearch: homeController.controllerSearch,
+              title: 'Pokedex',
+              onChanged: (_) => homeController.searchPokemon(),
+            ),
+            Expanded(
+              child: Observer(
+                builder: (_) {
+                  // Verifica se a lista está carregando
+                  if (!homeController.isLoadList) {
+                    return _buildLoadingIndicator();
+                  }
+
+                  // Verifica se a lista está vazia após a pesquisa
+                  if (homeController.listaPokemon.isEmpty) {
+                    return _buildEmptyState();
+                  }
+
+                  // Constrói a lista de pokémons
+                  return _buildPokemonList();
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
